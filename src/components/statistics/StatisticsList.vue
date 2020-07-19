42c682d2a80a754d84ebff8286ce733d5de321a5
gay0ung <6and_end9@naver.com>
2020-07-19 14:30:35 +0900

    create StatisticsPage
    create StatisticsPage.vue

---
+++ src/components/statistics/StatisticsList.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- 가영 -->
+  <!-- /statistics -->
+  <div class="statistics-list-cont">
+    <div class="statistics-list">
+      <ul>
+        <li>
+          <b>화장품</b>
+          <span>200,000 원</span>
+        </li>
+        <li>
+          <b>식비</b>
+          <span>100,000원</span>
+        </li>
+        <li>
+          <b>공과금</b>
+          <span>100,000원</span>
+        </li>
+        <li>
+          <b>교통비</b>
+          <span>100,000원</span>
+        </li>
+        <li>
+          <b>식비</b>
+          <span>100,000원</span>
+        </li>
+        <li>
+          <b>식비</b>
+          <span>100,000원</span>
+        </li>
+        <li>
+          <b>식비</b>
+          <span>100,000원</span>
+        </li>
+        <li>
+          <b>식비</b>
+          <span>100,000원</span>
+        </li>
+        <li>
+          <b>식비</b>
+          <span>100,000원</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/StatisticsPage.vue
+++ src/views/StatisticsPage.vue
@@ -5,11 +5,16 @@
 </template>
 
 <script>
-import StatisticsViewForm from '@/components/StatisticsViewForm.vue';
+import BarGraph from '@/components/statistics/BarGraph.vue';
+import RoundGraph from '@/components/statistics/RoundGraph.vue';
+import StatisticsList from '@/components/statistics/StatisticsList.vue';
+
 
 export default {
-  components: {
-    StatisticsViewForm,
+   components: {
+    BarGraph,
+    RoundGraph,
+    StatisticsList,
   },
 };
 </script>
