17df46e6634a073c242482e900a7518f5713069a
gay0ung <6and_end9@naver.com>
2020-07-24 14:44:11 +0900

    upddate btn css

--- src/components/AssetListForm.vue
+++ src/components/AssetListForm.vue
@@ -1,45 +1,65 @@
 <template>
-  <!-- 가영  mainpage -->
   <!-- /main  -->
   <div class="asset-list-form">
-    <!-- 총자산 1.현금카드전부더한거 2.현금따로 3.카드따로 -->
     <form @submit.prevent="submitForm">
+      <!-- 총자산 -->
       <div class="total-asset-cont">
         <strong>총 자산</strong>
-        <b class="ani-money">0000원</b>
+        <b id="ani-money">0000원</b>
       </div>
-
+      <!-- 카드 -->
       <div class="credit-asset-cont">
         <em>카드</em>
         <ul>
           <li>
             <span>신한카드</span>
-            <b class="ani-money">50,000원</b>
+            <b id="ani-money">50,000원</b>
           </li>
           <li>
             <span>하나카드</span>
-            <b class="ani-money">50,000원</b>
+            <b id="ani-money">50,000원</b>
           </li>
         </ul>
       </div>
-
+      <!-- 현금 -->
       <div class="cash-asset-cont">
         <em>현금</em>
-        <b class="ani-money">10,000 원</b>
+        <b id="ani-money">10,000 원</b>
       </div>
-      <button type="submit" class="btn"></button>
+
+      <button type="submit" class="btn big main">추가</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  // created(){
-  // },
+  data() {
+    return {};
+  },
+  created() {},
   methods: {
     submitForm() {
       this.$router.push('/daily');
-      console.log("it's works!");
+    },
+    aniMoney(time) {
+      const movingMoney = time.textContent;
+      console.log(movingMoney);
+
+      // time.textContent = '';
+
+      // for (str of movingMoney) {
+      //   let newI = document.createElement('i');
+
+      //   if (str === '') {
+      //     str = '&nbsp';
+      //   }
+      // }
+      // newI.innerHtml = str;
+
+      // delayTime = delayTime + time / movingMoney.length;
+      // newI.style.animationDelay = `${delayTime}s`;
+      // money.appendChild(newI);
     },
   },
 };
