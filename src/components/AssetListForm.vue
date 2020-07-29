42e4e97a6a5cbdb01db356758079dc99177ab90f
gay0ung <6and_end9@naver.com>
2020-07-29 11:39:16 +0900

    update AssetListForm

--- src/components/AssetListForm.vue
+++ src/components/AssetListForm.vue
@@ -5,22 +5,22 @@
       <!-- 총자산 -->
       <div class="total-asset-cont">
         <strong>총 자산</strong>
-        <b id="ani-money">{{}} 원</b>
+        <b id="ani-money">{{ makeComma(assetTotal) }} 원</b>
       </div>
       <!-- 카드 -->
       <div class="credit-asset-cont">
         <em>카드</em>
         <ul>
-          <li v-for="bInfo in bankAsset.bank" :key="bInfo.id">
-            <span>{{ bInfo }}</span>
-            <b id="ani-money">{{ bInfo.asset }}원</b>
+          <li v-for="nInfo in bankArr" :key="nInfo.id">
+            <span>{{ nInfo.bank }}</span>
+            <b id="ani-money">{{ makeComma(nInfo.asset) }}원</b>
           </li>
         </ul>
       </div>
       <!-- 현금 -->
       <div class="cash-asset-cont">
         <em>현금</em>
-        <b id="ani-money">{{ assetList.cash }} 원</b>
+        <b id="ani-money">{{ makeComma(cash) }} 원</b>
       </div>
 
       <button @click="addBtn" class="btn big main">추가</button>
@@ -29,33 +29,47 @@
 </template>
 
 <script>
-// import { addComma } from '@/utils/filters.js';
-import { getBanksCookie } from '@/utils/cookies.js';
+import { addComma } from '@/utils/filters.js';
 
 export default {
   data() {
     return {
       assetTotal: '',
-      assetList: {
-        total: this.$store.state.totalGoal,
-        cash: this.$store.state.cashGoal,
-        banks: [],
-      },
-      bankAsset: this.$store.state.bankAsset,
+      cash: this.$store.state.cashGoal,
+      bankArr: [],
     };
   },
   created() {
-    getBanksCookie(this.assetList);
+    let bank = this.$store.state.bankAsset.bank,
+      asset = this.$store.state.bankAsset.asset,
+      id = this.$store.state.bankAsset.id;
+
+    // 은행의 배열을 각각의 객체로 만드는 로직
+    for (let i = 0; i < bank.length; i++) {
+      this.bankArr.push({
+        bank: bank[i],
+        asset: asset[i],
+        id: id[i],
+      });
+    }
+
+    this.totalCalculate();
   },
-  comoputed: {
-    // assetCalculate() {
-    //   return this.assetTotal;
-    //   // assetTotal = assetList.cash +
-    // },
-  },
+  computed: {},
   methods: {
     addBtn() {
       this.$router.push('/daily');
+    },
+    makeComma(val) {
+      return addComma(val);
+    },
+    // 총 자산 계산
+    totalCalculate() {
+      let assets = this.$store.state.bankAsset.asset;
+
+      let totalBank = assets.reduce((a, b) => a * 1 + b * 1);
+
+      this.assetTotal = this.cash * 1 + totalBank;
     },
   },
 };
