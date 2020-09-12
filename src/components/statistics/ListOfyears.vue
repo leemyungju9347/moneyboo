770d5b7d80dbe88fd96eda5f8af64e587a0b3645
gay0ung <6and_end9@naver.com>
2020-09-12 14:53:17 +0900

    statisc Page

--- src/components/statistics/ListOfyears.vue
+++ src/components/statistics/ListOfyears.vue
@@ -3,48 +3,79 @@
     <div id="yearList" class="statistics-list">
       <ul>
         <li>
-          <b>화장품</b>
-          <span>200,000 원</span>
-        </li>
-        <li>
-          <b>화장품</b>
-          <span>200,000 원</span>
-        </li>
-        <li>
-          <b>화장품</b>
-          <span>200,000 원</span>
-        </li>
-        <li>
-          <b>화장품</b>
-          <span>200,000 원</span>
-        </li>
-        <li>
-          <b>화장품</b>
-          <span>200,000 원</span>
-        </li>
-        <li>
-          <b>화장품</b>
-          <span>200,000 원</span>
-        </li>
-        <li>
-          <b>화장품</b>
-          <span>200,000 원</span>
-        </li>
-        <li>
-          <b>화장품</b>
-          <span>200,000 원</span>
-        </li>
-        <li>
-          <b>화장품</b>
-          <span>200,000 원</span>
+          <b>{{}}</b>
+          <span>{{}} 원</span>
         </li>
       </ul>
     </div>
+    {{ yearsExpend }}
   </div>
 </template>
 
 <script>
-export default {};
+import { addComma } from '@/utils/filters';
+import { moneybooRef } from '@/api/firestore';
+export default {
+  data() {
+    return {
+      currentUID: '',
+      yearsList: [],
+      yearsExpend: [],
+    };
+  },
+  created() {
+    this.currentUID = this.$store.state.uid;
+    // this.getDailyList();
+    this.getDailyDB();
+    addComma();
+  },
+  methods: {
+    //1. 데이터를 폴더 별로 불러온다
+    //2. 해당 폴더의 수입 지출 을 각각 계산해준다(한달 기준)(일단 지출을 해본후 구현가능 하다면 수입항목도 넣어보자.)
+    //3. 해당 월을 클릭했을경우 round-graph에 반영이 되게 해준다.
+    //4. plugin에 적용을 시킨다.
+    //5. 폴더명앞에  년도의 뒷부분과 일치한 폴더 명만 가져온다. (20.08폴더 명이 이렇게 되어있음.)
+    mBooRef() {
+      return moneybooRef(this.currentUID);
+    },
+    getDailyDB() {
+      this.mBooRef()
+        .doc('daily')
+        .collection('listAdd')
+        .onSnapshot(snapShot => {
+          snapShot.forEach(doc => {
+            this.yearsList.push(doc.data().listData);
+          });
+          this.calculateEP();
+        });
+    },
+    // 1년 중 월별 총 지출
+    calculateEP() {
+      const yearsDB = this.yearsList;
+      let expendLT = [];
+      let expendList = {
+        month: 0,
+        expend: 0,
+      };
+      for (let i = 0; i < yearsDB.length; i++) {
+        yearsDB[i].map(el => {
+          if (el.item === 'expend') {
+            const price = el.price;
+            const date = el.date.substr(0, 1);
+
+            if (date) {
+              expendList.month = Number(date);
+              expendList.expend += Number(price);
+            }
+          }
+        });
+        expendLT.push(expendList);
+        // console.log(expendLT);
+        // log를 찍을 때에는 월별로 객체에 나뉘어 나왔는데 왜 배열에 담으면9 월의 값만 담기는 거지..?
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/components/statistics/RoundGraph.vue
+++ src/components/statistics/RoundGraph.vue
@@ -3,46 +3,52 @@
     <div class="round-graph">
       <h3>{{ whatMonth() }}월</h3>
       <div id="canvas-holder">
-        <canvas id="r-graph"></canvas>
+        <canvas ref="rGraph"></canvas>
       </div>
     </div>
-    <ListOfmonth :monthCk="whatMonth()"></ListOfmonth>
+
+    <ListOfMonth :monthCk="whatMonth" @listM-data="cList"></ListOfMonth>
+    {{ pPrice }}
   </div>
 </template>
 
 <script>
-import Chart from 'chart.js';
-import ListOfmonth from './ListOfMonth';
 import { todayCheck } from '@/utils/statistics.js';
+import { addComma } from '@/utils/filters';
+import ListOfMonth from '@/components/statistics/ListOfMonth.vue';
+import { eventBus } from '../../main';
 
 export default {
   components: {
-    ListOfmonth,
+    ListOfMonth,
   },
   data() {
     return {
-      dailyList: this.$store.state.listData,
+      cList: [],
+      pPrice: [],
     };
   },
-  methods: {
-    whatMonth() {
-      return todayCheck();
-    },
+  created() {
+    eventBus.$once('sM-data', cList => {
+      this.cList = cList;
+      this.Percentage();
+    });
   },
   mounted() {
-    const ctx = document.getElementById('r-graph');
+    console.log('this is mounted');
+    const ctx = this.$refs.rGraph;
 
-    // chart default값 설정하기
-    Chart.defaults.global.defaultFontColor = '#3b3b3b';
-    Chart.defaults.global.defaultFontFamily = 'Jua';
+    //chart default값 설정하기
+    this.$_Chart.defaults.global.defaultFontColor = '#3b3b3b';
+    this.$_Chart.defaults.global.defaultFontFamily = 'Jua';
 
-    let myChart = new Chart(ctx, {
+    let myChart = new this.$_Chart(ctx, {
       type: 'pie',
       data: {
-        // 이름.
-        labels: ['화장품', '식비', '공과금', '교통비', '식비'],
+        labels: ['sdf', 'zc', 'iwuqe'], // 마우스가 근처에 오면 해당 카테고리 이름이 나온다.
         datasets: [
           {
+            data: [],
             backgroundColor: [
               'rgb(224, 242, 241)',
               'rgb(178, 223, 219)',
@@ -50,7 +56,6 @@ export default {
               'rgb(77, 182, 172)',
               'rgb(38, 166, 154)',
             ],
-            data: [10, 20, 40, 10, 20],
             borderWidth: 0.5,
             borderAlign: 'innner',
           },
@@ -58,25 +63,60 @@ export default {
       },
 
       options: {
-        layout: {},
+        // 추가하고 싶은 옵션이 있을경우 사용
         responsive: true,
         maintainAspectRatio: false,
-        legend: {
-          display: false,
-          position: 'bottom',
-          labels: {
-            //   fontColor: '#3b3b3b',
-            //   fontFamily: 'BBTreeGR',
-            // boxWidth: 14,
-            // fontSize: 12,
-            // padding: 8,
-            //   fontStyle: 'bold',
-          },
+        legend: false, // label의 색상을 알려주는 것.
+
+        pieceLabel: {
+          mode: 'label',
+          position: 'border',
+          fontSize: 11,
+          fontStyle: 'bold',
         },
+        animation: {
+          animateScale: true,
+        },
+
+        scales: {},
       },
     });
+    /*** 해결이 필요한 부분 , 생성주기의 문제 같은데 아직 찾지를 못하겠다. ***/
+    //data 값 setting하기
+    //console.log(this.pPrice); //[__ob__: Observer]
+    //console.log(this.cList); //[__ob__: Observer]
+    console.log(this.pPrice);
+    myChart.data.datasets[0].data = this.pPrice;
+    myChart.data.labels = Object.keys(this.cList);
 
+    myChart.update();
     return myChart;
+  },
+
+  methods: {
+    whatMonth() {
+      return todayCheck();
+    },
+    addComma() {
+      return addComma;
+    },
+    Percentage() {
+      const cList = this.cList;
+      let pieP = [];
+      let EPprice = [];
+      let totalNum = 0;
+
+      for (let key in cList) {
+        let price = cList[key].price;
+        totalNum += price;
+        EPprice.push(price);
+      }
+
+      EPprice.sort((a, b) => a - b);
+      EPprice.filter(el => pieP.push(Math.round((el / totalNum) * 100)));
+
+      pieP = this.pPrice;
+    },
   },
 };
 </script>
